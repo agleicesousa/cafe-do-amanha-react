82e2217823ea2ba1b3953dccaf086c60872d5556
agleicesousa <aleicesousa98@gmail.com>
2025-01-23 03:01:05 -0300

    feat: est inicial para a página de 'contatos'

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./Components/GlobalStye/globalStye.css";
 import Header from "./Components/Header/header";
+import Contatos from "./Pages/Contatos/contatos";
 import Inicio from "./Pages/Inicio/inicio";
 import Footer from "./Components/Footer/footer";
 
@@ -11,6 +12,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/contatos" element={<Contatos />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/Pages/Contatos/contato.jsx
@@ -0,0 +1,10 @@
+import s from "./contato.module.css";
+
+export default function Contatos() {
+  return (
+    <main>
+      <h1 className={s.titulo}>Contatos</h1>
+      <p>teste</p>
+    </main>
+  );
+}
