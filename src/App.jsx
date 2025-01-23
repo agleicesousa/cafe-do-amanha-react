d22b0f48816ad3e9b73f561ffa02f0289e8b25f7
agleicesousa <aleicesousa98@gmail.com>
2025-01-23 03:17:25 -0300

    feat: est. inicial da página 'sobre nós'

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Contatos from "./Pages/Contatos/contatos";
 import Historias from "./Pages/Historias/historias";
 import Inicio from "./Pages/Inicio/inicio";
 import Menu from "./Pages/Menu/menu";
+import SobreNos from "./Pages/SobreNos/sobreNos";
 import Footer from "./Components/Footer/footer";
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/historias" element={<Historias />} />
         <Route path="/" element={<Inicio />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="/sobre-nos" element={<SobreNos />} />
       </Routes>
       <Footer />
     </BrowserRouter>
