0a44fb02a3aa646cc8de8bf1f812853ac0fc927a
agleicesousa <aleicesousa98@gmail.com>
2025-01-23 02:55:42 -0300

    refactor: melhoria no exemplo de uso dos 'modules' na estilização

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -2,8 +2,9 @@ import s from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <footer className={s.footer}>
-      <h1>Footer</h1>
+    <footer>
+      <h1 className={s.titulo}>Footer</h1>
+      <p>teste</p>
     </footer>
   );
 }
--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -2,8 +2,9 @@ import s from "./header.module.css";
 
 export default function Header() {
   return (
-    <header className={s.header}>
-      <h1>Header</h1>
+    <header>
+      <h1 className={s.titulo}>Header</h1>
+      <p>teste</p>
     </header>
   );
 }
