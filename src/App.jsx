5c1539d3cf57419449b6d1bc46ff93eaba78b125
agleicesousa <aleicesousa98@gmail.com>
2025-01-23 02:39:44 -0300

    feat: arq. iniciais do componente 'header'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./Components/GlobalStye/globalStye.css";
+import Header from "./Components/Header/header";
 import Inicio from "./Pages/Inicio/inicio";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Inicio />} />
       </Routes>
---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,9 @@
+import s from "./header.module.css";
+
+export default function Header() {
+    return (
+        <header className={s.header}>
+            <h1>Header</h1>
+        </header>
+    );
+}
