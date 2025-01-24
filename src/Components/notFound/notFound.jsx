7f43353551d4af77c16e5fc5bee2f12e26f81f40
agleicesousa <aleicesousa98@gmail.com>
2025-01-24 16:07:23 -0300

    chrore: removendo classes desnecessárias - NotFound

--- src/Components/notFound/notFound.jsx
+++ src/Components/notFound/notFound.jsx
@@ -3,8 +3,8 @@ import s from"./notFound.module.css";
 export default function NotFound() {
   return (
     <main className={s.container_notFound}>
-      <h1 className={s.titulo_notFound}>Página Não Encontrada</h1>
-      <p className={s.paragrafo_notFound}>Ops! A página que você está procurando não existe.</p>
+      <h1>Página Não Encontrada</h1>
+      <p>Ops! A página que você está procurando não existe.</p>
     </main>
   );
 }
