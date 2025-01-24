c96b81ebb295c6098a0f259043472397f3703b22
agleicesousa <aleicesousa98@gmail.com>
2025-01-24 16:03:15 -0300

    feat: usado 'module' - NorFound

--- src/Components/notFound/notFound.jsx
+++ src/Components/notFound/notFound.jsx
@@ -1,10 +1,10 @@
-import s from "./notFound.module.css";
+import s from"./notFound.module.css";
 
-export default function Header() {
+export default function NotFound() {
   return (
-    <main>
-      <h1 className={s.titulo}>Not Found</h1>
-      <p>teste</p>
+    <main className={s.container_notFound}>
+      <h1 className={s.titulo_notFound}>Página Não Encontrada</h1>
+      <p className={s.paragrafo_notFound}>Ops! A página que você está procurando não existe.</p>
     </main>
   );
 }
