c2048c8b0dd1a4b3623cd512826f4f44971d01e7
agleicesousa <aleicesousa98@gmail.com>
2025-01-24 14:12:07 -0300

    feat: est. inicial para o componente 'Not Found'

---
+++ src/Components/notFound/notFound.jsx
@@ -0,0 +1,10 @@
+import s from "./notFound.module.css";
+
+export default function Header() {
+  return (
+    <main>
+      <h1 className={s.titulo}>Not Found</h1>
+      <p>teste</p>
+    </main>
+  );
+}
