431fb3cf3d56a1fd877fa1b2ac5d0104619fb5f9
agleicesousa <aleicesousa98@gmail.com>
2025-01-24 16:32:39 -0300

    feat: add tag 'section' dentro da main

--- src/Components/notFound/notFound.jsx
+++ src/Components/notFound/notFound.jsx
@@ -3,8 +3,10 @@ import s from"./notFound.module.css";
 export default function NotFound() {
   return (
     <main className={s.container_notFound}>
-      <h1>Página Não Encontrada</h1>
-      <p>Ops! A página que você está procurando não existe.</p>
+      <section>
+        <h1>Página Não Encontrada</h1>
+        <p>Ops! A página que você está procurando não existe.</p>
+      </section>
     </main>
   );
 }
