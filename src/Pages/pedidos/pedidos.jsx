355eb570408afd5b79c190245e3d8ea3a72db555
agleicesousa <aleicesousa98@gmail.com>
2025-01-25 02:23:34 -0300

    feat: estilização geral da página 'pedidos'

--- src/Pages/pedidos/pedidos.jsx
+++ src/Pages/pedidos/pedidos.jsx
@@ -2,49 +2,46 @@ import s from "./pedidos.module.css";
 
 export default function Pedidos() {
   return (
-    <>
-      <main className={s.main_pedidos}>
-        <section className={s.section_pedidos}>
-          <h1 className={s.titulo_pedidos}>Faça seu pedido</h1>
-          <section className={s.container_pedidos}>
+    <main className={s.main_pedidos}>
+      <section className={s.section_pedidos}>
+        <h1 className={s.titulo_pedidos}>Faça seu pedido</h1>
+        <section className={s.container_pedidos}>
 
-            <div className={s.cliente_pedidos}>
-              <section className={s.section_name}>
-                <label htmlFor="name">Nome:</label>
-                <input className={s.input_name} type="text" name="name" />
-              </section>
-              <section className={s.section_mesa}>
-                <label htmlFor="mesa">Número da mesa:</label>
-                <input className={s.input_mesa} type="number" name="mesa" />
-              </section>
-            </div>
+          <div className={s.cliente_pedidos}>
+            <section className={s.section_name}>
+              <label htmlFor="name">Nome:</label>
+              <input id="name" className={s.input_name} type="text" name="name" />
+            </section>
+            <section className={s.section_mesa}>
+              <label htmlFor="mesa">Número da mesa:</label>
+              <input id="mesa" className={s.input_mesa} type="number" name="mesa" />
+            </section>
+          </div>
 
-            <div className={s.produtos_pedidos}>
-              <section className={s.section_produtos_pedidos}>
-                <button className="btn_produtos_pedidos">Cafés</button>
-                <button className="btn_produtos_pedidos">SObremesas</button>
-              </section>
-              <section className={s.section_produtos_pedidos}>
-                <button className="btn_produtos_pedidos">Especiais</button>
-                <button className="btn_produtos_pedidos">Bebidas Geladas</button>
-              </section>
-              <section className={s.section_produtos_pedidos}>
-                <button className="btn_produtos_pedidos">Chás</button>
-              </section>
-            </div>
+          <div className={s.produtos_pedidos}>
+            <section className={s.section_produtos}>
+              <button className={s.btn_produtos}>Cafés</button>
+              <button className={s.btn_produtos}>Sobremesas</button>
+            </section>
+            <section className={s.section_produtos}>
+              <button className={s.btn_produtos}>Especiais</button>
+              <button className={s.btn_produtos}>Bebidas Geladas</button>
+            </section>
+            <section className={s.section_produtos}>
+              <button className={s.btn_produtos}>Chás</button>
+            </section>
+          </div>
 
-            <div className={s.total_pedidos}> 
-              <section className={s.section_total}>
-                <label htmlFor="total">Total:</label>
-                <h2>R$ 0</h2>
-              </section>
-              <section className={s.section_botao}>
-                <button className="btn_total_pedidos">Finalizar Pedido</button>
-              </section>
-            </div>
-          </section>
+          <div className={s.total_pedidos}> 
+            <section className={s.section_total}>
+              <h2>Total:</h2>
+              <h2>R$ 0</h2>
+            </section>
+            <button className={s.btn_total}>Finalizar Pedido</button>
+          </div>
+          
         </section>
-      </main>
-    </>
+      </section>
+    </main>
   );
 }
