28d7b5264806b379c46ebc3520f7375b10b5ca21
agleicesousa <aleicesousa98@gmail.com>
2025-01-25 05:10:01 -0300

    feat: est. inicial do componente 'modal'

---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,38 @@
+import s from "./Modal.module.css";
+
+export default function Modal() {
+  return (
+    <section className={s.modal}>
+      <section className={s.container_modal} id="modal">
+        <section className={s.section_modal}>
+          <h2>Selecione o ítem e a quantidade</h2>
+          <div className={s.produtos_modal}>
+            <section className={s.section_produtos_modal}>
+              <div className={s.item_modal}>
+                <h3>Item:</h3>
+                <form action="itens">
+                  <select name="itens" id="itens">
+                    <option value="selecione">Selecione um item</option>
+                    <option value="cafe">Café</option>
+                    <option value="sobremesa">Sobremesa</option>
+                    <option value="especial">Especial</option>
+                    <option value="bebida">Bebida</option>
+                    <option value="chas">Chás</option>
+                  </select>
+                </form>
+              </div>
+            </section>
+            <section className={s.section_quantidade_modal}>
+              <h3>Quantidade:</h3>
+              <input type="number" name="quantidade" id="quantidade" />
+            </section>
+            <section className={s.btn_modal}>
+              <button className={s.btn_cancelar}>Cancelar</button>
+              <button className={s.btn_adicionar}>Adicionar ao pedido</button>
+            </section>
+          </div>
+        </section>
+      </section>
+    </section>
+  );
+}
