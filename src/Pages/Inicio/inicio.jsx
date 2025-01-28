e6e4255243418bf82918ae9fadd3c50146848c48
agleicesousa <aleicesousa98@gmail.com>
2025-01-28 18:15:06 -0300

    refactor: ajustando cores e classes na página de inicio
    Co-authored-by: Thales-hop <kauathales091@gmail.com>

--- src/Pages/Inicio/inicio.jsx
+++ src/Pages/Inicio/inicio.jsx
@@ -2,9 +2,9 @@ import s from "./inicio.module.css";
 
 export default function Inicio() {
     return (
-        <main className={s.sMain}>
-            <section id="inicio" className={s.sInicio}>
-                <div className={s.sInicio__intro}>
+        <main className={s.main_inicio}>
+            <section id="inicio" className={s.section_inicio}>
+                <div className={s.inicio__intro}>
                     <h2>Bem-vindo ao nosso Café da Manhã!</h2>
                     <p>Aqui você encontrará histórias e delícias de dar água na boca. Aproveite!</p>
                 </div>
