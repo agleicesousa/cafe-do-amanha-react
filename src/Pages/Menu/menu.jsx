070c29719aec0f40289261e1ff102a882cb38357
agleicesousa <aleicesousa98@gmail.com>
2025-01-29 02:37:44 -0300

    feat: est. inicial da página de menu
    Co-authored-by: Steicie Aleixo Sousa <51095043+Staici@users.noreply.github.com>

--- src/Pages/Menu/menu.jsx
+++ src/Pages/Menu/menu.jsx
@@ -1,10 +1,26 @@
 import s from "./menu.module.css";
+// import menuItems from "../../mocks/menuItens.json";
 
 export default function Menu() {
   return (
-    <main>
-      <h1 className={s.titulo}>Menu</h1>
-      <p>teste</p>
+    <main className={s.main_menu}>
+      <section className={s.section_menu}>
+        <h1 className={s.titulo_menu}>Café do Amanhã</h1>
+        <img src="#" alt="#" />
+      </section>
+      <section className={s.opcoes_menu}>
+        <div className={s.lista_opcao_menu}>
+          <nav>
+            <ul>
+              <li>algo aqui</li>
+            </ul>
+          </nav>
+        </div>
+        <div className={s.lista_itens_menu}>
+          <h3 className={s.titulo_item}>algo aqui</h3>
+          <p className={s.preco_item}>R$ 00,00</p>
+        </div>
+      </section>
     </main>
   );
 }
