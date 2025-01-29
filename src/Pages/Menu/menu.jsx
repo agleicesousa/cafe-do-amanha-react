a62c524678d799c57dc1092902ab092e5802a965
agleicesousa <aleicesousa98@gmail.com>
2025-01-29 03:02:29 -0300

    feat: adicionando lógica na página 'menu' para usar o arquivo JSON
    Co-authored-by: Steicie Aleixo Sousa <51095043+Staici@users.noreply.github.com>

--- src/Pages/Menu/menu.jsx
+++ src/Pages/Menu/menu.jsx
@@ -1,28 +1,70 @@
+import { useState } from "react";
 import s from "./menu.module.css";
-// import menuItems from "../../mocks/menuItens.json";
+import menuItems from "../../mocks/menuItens.json";
 
 export default function Menu() {
+  const [category, setCategory] = useState("cafes");
+  const [mainImage, setMainImage] = useState(
+    menuItems["cafes"][Object.keys(menuItems["cafes"])[0]]
+  );
+
+  const handleCategoryChange = (category) => {
+    setCategory(category);
+    setMainImage(menuItems[category][Object.keys(menuItems[category])[0]]);
+  };
+
+  const handleItemClick = (item) => {
+    setMainImage(item);
+  };
+
   return (
     <main className={s.main_menu}>
       <section className={s.section_menu}>
         <h1 className={s.titulo_menu}>Café do Amanhã</h1>
-        <img src="#" alt="#" />
+        <img src={mainImage.image} alt={mainImage.alt} />
       </section>
-      <section className={s.opcoes_menu}>
-        <div className={s.lista_opcao_menu}>
-          <nav>
-            <ul>
-              <li><button>Cafés</button></li>
-              <li><button>Sobremesas</button></li>
-              <li><button>Especiais</button></li>
-              <li><button>Bebidas</button></li>
-              <li><button>Chás</button></li>
-            </ul>
-          </nav>
-        </div>
+      <section className={s.section_opcoes_menu}>
+        <nav className={s.nav_opcao_menu}>
+          <ul>
+            <li>
+              <button onClick={() => handleCategoryChange("cafes")}>
+                Cafés
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleCategoryChange("sobremesas")}>
+                Sobremesas
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleCategoryChange("especiais")}>
+                Especiais
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleCategoryChange("bebidasGeladas")}>
+                Bebidas
+              </button>
+            </li>
+            <li>
+              <button onClick={() => handleCategoryChange("chas")}>Chás</button>
+            </li>
+          </ul>
+        </nav>
         <div className={s.lista_itens_menu}>
-          <h3 className={s.titulo_item}>algo aqui</h3>
-          <p className={s.preco_item}>R$ 00,00</p>
+          {Object.keys(menuItems[category]).map((itemKey) => {
+            const item = menuItems[category][itemKey];
+            return (
+              <div
+                className={s.item_menu}
+                key={item.id}
+                onClick={() => handleItemClick(item)}
+              >
+                <h3 className={s.titulo_item}>{itemKey}</h3>
+                <p className={s.preco_item}>R$ {item.price.toFixed(2)}</p>
+              </div>
+            );
+          })}
         </div>
       </section>
     </main>
