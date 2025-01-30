aeaf6ae0b6078342b8a64b909986186a324c18e7
agleicesousa <aleicesousa98@gmail.com>
2025-01-30 01:34:07 -0300

    fix: est. prettier - footer

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -3,9 +3,12 @@ import s from "./footer.module.css";
 const getYear = () => new Date().getFullYear();
 
 export default function Footer() {
-    return (
-        <footer className={s.footer}>
-            <p>&copy; {getYear()} Café da Manhã com história. Todos os direitos reservados.</p>
-        </footer>
-    );
+  return (
+    <footer className={s.footer}>
+      <p>
+        &copy; {getYear()} Café da Manhã com história. Todos os direitos
+        reservados.
+      </p>
+    </footer>
+  );
 }
