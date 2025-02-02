b00b3fb237a82076201ffd18710a619445755f29
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-02-02 01:24:33 -0300

    feat/refactor: responsividade no header e padrão css

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,15 +1,28 @@
 import s from "./header.module.css";
 import { NavLink } from "react-router-dom";
 import Logo from "/assets/coffee.png";
+import { useState } from "react";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const closeMenu = () => setIsMenuOpen(false);
+
   return (
     <header className={s.header}>
       <section className={s.logo}>
         <img src={Logo} alt="Logo da página" />
         <h1 className={s.titulo_header}>Café do Amanhã</h1>
       </section>
-      <section className={s.nav_header}>
+
+      {/* Fundo escuro (overlay) */}
+      <div
+        className={`${s.overlay} ${isMenuOpen ? s.show : ""}`}
+        onClick={closeMenu}
+      ></div>
+
+      {/* Menu lateral */}
+      <section className={`${s.nav_header} ${isMenuOpen ? s.open : ""}`}>
         <ul className={s.nav_menu}>
           <li>
             <NavLink
@@ -61,7 +74,13 @@ export default function Header() {
           </li>
         </ul>
       </section>
+
+      {/* Ícone do burger */}
+      <div className={s.burger} onClick={toggleMenu}>
+        <div className={s.burger_line}></div>
+        <div className={s.burger_line}></div>
+        <div className={s.burger_line}></div>
+      </div>
     </header>
   );
 }
-
