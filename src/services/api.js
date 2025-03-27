34cf1db39fb72c323f64bb537443caf008fe7773
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2025-03-27 19:59:16 -0300

    refactor: remover código não utilizado e otimizar a função de criação de cliente

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,3 @@
-//! Ainda não está sendo utilizado
 const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
 export const fetchItemsByCategory = async (category) => {
@@ -6,25 +5,6 @@ export const fetchItemsByCategory = async (category) => {
         const response = await fetch(`${API_URL}/menu/itens/categoria/${category}`);
         if (!response.ok) {
             throw new Error('Erro ao buscar itens');
-        }
-        return await response.json();
-    } catch (error) {
-        console.error('Erro na requisição:', error);
-        throw error;
-    }
-};
-
-export const createClient = async (clientData) => {
-    try {
-        const response = await fetch(`${API_URL}/clientes`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(clientData),
-        });
-        if (!response.ok) {
-            throw new Error('Erro ao criar cliente');
         }
         return await response.json();
     } catch (error) {
