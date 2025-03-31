aeaff9d95384717fb13cf8a6c64794d765c045a2
rafaelsr20 <rafaeldesr20@gmail.com>
2025-03-31 09:59:03 -0300

    Atualização de pasta

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,25 @@
-//! Ainda não está sendo utilizado
-// const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
 
-//*Função para tratar respostas da API - caso necessário
-// const handleResponse = async (response) => {
-//   if (!response.ok) {
-//     const errorData = await response.json().catch(() => ({}));
-//     throw new Error(errorData.error || 'Erro na requisição');
-//   }
-//   return response.json();
-// };
-
-//* exemplo de uso em um componente
-// export const listarPedidos = async () => {
-//     const response = await fetch(`${API_URL}/pedidos`);
-//     return handleResponse(response);
-//   };
+const handleResponse = async (response) => {
+    if (!response.ok) {
+      const errorData = await response.json().catch(() => ({}));
+      throw new Error(errorData.error || 'Erro na requisição');
+    }
+    return response.json();
+  };
+// listar todos os itens do menu
+export const listarTodosItens = async () => {
+  const response = await fetch(`${API_URL}/menu`);
+  return handleResponse(response); 
+};
+// listar por id
+export const buscarItemPorId = async (id) => {
+    const response = await fetch(`${API_URL}/menu/${id}`); 
+    return handleResponse(response); 
+};
+  // listar por categoria
+export const buscarItensPorCategoria = async (categoria) => {
+    const response = await fetch(`${API_URL}/menu?categoria=${categoria}`); 
+    return handleResponse(response);
+};
+  
